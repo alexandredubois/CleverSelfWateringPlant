7f11658187625af2fd2eba75afcddebe2a7bda1c
Alexandre Dubois <contact@alexandredubois.com>
2012-11-20 20:32:26 +0100

    MAJ comment

--- ArrosageAutomatique.ino
+++ ArrosageAutomatique.ino
@@ -1,12 +1,16 @@
 /*
-  Self-Watering Plant
-  by Randy Sarafan
+  (Clever)Self-Watering Plant
+  by Alexandre Dubois
+  inspired by Randy Sarafan
   
-  Reads a soil moisture sensor and turns on a relay that controls a water pump.
+  Reads a soil moisture sensor and a LDR and turns on a relay that controls a water pump.
   
-  The soil moisture sensor involves a 10K resistor between pins A1 and ground,
+  The soil moisture sensor involves a 10K resistor between pins A1 and ground
   and a probe connected to pin A1 and another connected to +5V. These probes 
   are embedded and inch apart in the plant's soil.
+  
+  The light sensor involves a 2,2K resistor between pins A2 and ground. The LDR is connected
+  both to the A2 pin and the +5V.
   
   For more information, check out:
   http://www.instructables.com/id/Self-Watering-Plant/
@@ -23,7 +27,7 @@ int lightValue = 0;
 
  // if the readings from the soil sensor drop below this number, then turn on the pump
 int dryValue = 700;
-int darkValue = 50;
+int darkValue = 500;
 
 void setup() {
   
