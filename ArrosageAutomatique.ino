26883474450e03cf1105c28ede40672aa6cb83fa
Alexandre Dubois <contact@alexandredubois.com>
2013-03-20 11:11:02 +0100

    Refacto du code, ajout des leds de statut et de l'étape d'initialisation

--- ArrosageAutomatique.ino
+++ ArrosageAutomatique.ino
@@ -21,6 +21,10 @@ ISR(WDT_vect) {
   Sleepy::watchdogEvent(); 
 } // Setup for low power waiting
 
+/************************
+*** CONST & VARIABLES ***
+************************/
+
 const int probesInPin = A1;//Pin attached to the probes
 const int lightInPin = A2;//Pin attached to the LDR
 const int potInPin = A5;//Pin attached to the potentiometer
@@ -29,40 +33,37 @@ const int pumpOutPin = 12;//Pin attached to the water pump
 const int greenOutPin = 7;
 const int redOutPin = 8;
 
-const int debug = 1;
+const int debug = 0;
 
-// value read from the soil moisture sensor
 int probesTreshold = 0;
 int probesValue = 0;
 int lightValue = 0;
 
 const int loopCount = 2;
 int timeCount = 1;
-
-// if the readings from the soil sensor drop below this number, then turn on the pump
-const int dryValue = 700;
+const int hysteresis = 100;
 const int darkValue = 300;
 
-void setup()
-{
-  pinMode(greenOutPin, OUTPUT);
-  pinMode(redOutPin, OUTPUT);
-  
-  if(debug == 1){
-    Serial.println("Initialising...");
-    Serial.begin(9600);
-    Serial.println("Initialisation complete.");
-  }
-}
+/****************
+*** FUNCTIONS ***
+****************/
 
 //Check if the plant needs water !
-void water()
+void monitorPlantStatus()
 {
   readProbesValue();
   readLightValue();
-  //Turns on the water pump if the soil is too dry
-  //Increasing the delay will increase the amount of water pumped
-  if(probesValue < dryValue && lightValue < darkValue){
+  if(probesValue < probesTreshold && lightValue < darkValue)
+  {
+    water();
+  }
+} 
+
+//Turn on the pump during a few seconds
+//Increasing the delay will increase the amount of water pumped
+void water()
+{
+    //Turns on the water pump if the soil is too dry
     if(debug == 1)
     {
       Serial.println("Pump is ON !");
@@ -79,6 +80,7 @@ void water()
   }
 }
 
+/*Read potentiometer value*/
 void readPotValue()
 {
   probesTreshold = analogRead(potInPin);
@@ -90,6 +92,7 @@ void readPotValue()
   }
 }
 
+/*Read moisture soil value from the probes*/
 void readProbesValue()
 {
   probesValue = analogRead(probesInPin);
@@ -100,6 +103,7 @@ void readProbesValue()
   }
 }
 
+/*Read light walue received by the LDR*/
 void readLightValue()
 {
   lightValue = analogRead(lightInPin);
@@ -110,44 +114,95 @@ void readLightValue()
   }
 }
 
-void manageTime()
+/*Let the user define its ideal moisture level slowly blinking led*/
+void tresholdSetup()
+{
+  for (int i=0;i<=100;i++)
+  {
+    readPotValue();
+    readProbesValue();
+    if(probesTreshold < (probesValue - hysteresis))
+    {
+      digitalWrite(redOutPin, HIGH);
+      digitalWrite(greenOutPin, LOW);
+    }
+    else if(probesTreshold > (probesValue + hysteresis))
+    {
+      digitalWrite(redOutPin, LOW);
+      digitalWrite(greenOutPin, HIGH);
+    }
+    else {
+      digitalWrite(redOutPin, HIGH);
+      digitalWrite(greenOutPin, HIGH);
+    }
+    Sleepy::loseSomeTime(300);
+  }
+}
+
+/*Blink leds to warn the user before launching the main program*/
+void launchWarning()
+{
+  for (int i=0;i<=6;i++)
+  {
+    digitalWrite(redOutPin, HIGH);
+    digitalWrite(greenOutPin, HIGH);
+    Sleepy::loseSomeTime(300);
+    digitalWrite(redOutPin, LOW);
+    digitalWrite(greenOutPin, LOW);
+    Sleepy::loseSomeTime(300);
+  }
+}
+
+/*Flash the green led quickly to show the unit is working*/
+void blinkStatusLed()
+{
+  pinMode(greenOutPin, OUTPUT);
+  digitalWrite(greenOutPin, HIGH);
+  delay(100);
+  digitalWrite(greenOutPin, LOW);
+  pinMode(greenOutPin, INPUT);
+}
+
+/***************************
+*** SETUP & MAIN PROGRAM ***
+***************************/
+
+/*Setup the device*/
+void setup()
+{
+  pinMode(greenOutPin, OUTPUT);
+  pinMode(redOutPin, OUTPUT);
+  tresholdSetup(); //Setup the moisture level
+  launchWarning(); //Warn the user
+  pinMode(greenOutPin, INPUT);
+  pinMode(redOutPin, INPUT);
+
+  if(debug == 1){
+    Serial.println("Initialising...");
+    Serial.begin(9600);
+    Serial.println("Initialisation complete.");
+  }
+}
+
+/*Main program : wait and check the plant every X minutes*/
+void loop () 
 {
   if(timeCount >= loopCount){
     if(debug == 1)
     {
       Serial.println("Check plant");
     }
-    water();
-    //Reset time counter
-    timeCount = 0;
+    monitorPlantStatus();
+    timeCount = 0; //Reset time counter
   }
   //Wait more...
   else{
     if(debug == 1)
     {
       Serial.println("Wait !");
-      Sleepy::loseSomeTime(300);
+      delay(300);
     }
     ++timeCount;
   }
-
-  Sleepy::loseSomeTime(10000);// Instead of delay(1000); 
-}
-
-
-void loop () 
-{
-  readPotValue();
-  if(probesTreshold < 700)
-  {
-    digitalWrite(redOutPin, HIGH);
-    digitalWrite(greenOutPin, LOW);
-  }
-  else {
-    digitalWrite(redOutPin, LOW);
-    digitalWrite(greenOutPin, HIGH);
-  }
-  Sleepy::loseSomeTime(500);
-  //manageTime();
+  Sleepy::loseSomeTime(60000);// Wait a minute
 } // end of loop
-
